9d41f1d935e0b53ac36939c5b80cfb4c47e0f142
niceprogramming <niceprogramming@github.com>
2019-08-25 14:38:54 -0500

    body parser

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import api from './routes/api';
 import logger from './utils/logger';
 
 const app = express();
 const port = 3000;
-
+app.use(bodyParser.json());
 app.use('/api', api);
 
 app.listen(port, () => logger.info(`Example app listening on port ${port}!`));
